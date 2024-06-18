fb173468d2dbe19af431a69dcd7494a935617c7b
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-06-18 21:21:28 +0530

    db update

---
+++ databases/db.php
@@ -0,0 +1,29 @@
+<?php
+function loadEnv($path)
+{
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $vars = [];
+
+    foreach ($lines as $line) {
+        if (strpos($line, '=') !== false) {
+            list($name, $value) = explode('=', $line, 2);
+            $vars[$name] = $value;
+        }
+    }
+
+    return $vars;
+}
+
+$env = loadEnv(__DIR__ . '/.env');
+
+$host = $env['DB_HOST'];
+$username = $env['DB_USER'];
+$password = $env['DB_PASS'];
+$dbname = $env['DB_NAME'];
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
